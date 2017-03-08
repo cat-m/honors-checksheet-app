752730f2af19fa38441563e41dfa1077121296a7
apontive <apontive@mail.umw.edu>
2017-03-08 12:46:06 +0000

    Add new html template

--- server.py
+++ server.py
@@ -4,8 +4,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def helloWorld():
+def index():
     return render_template("index.html")
+  
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+    
     
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
