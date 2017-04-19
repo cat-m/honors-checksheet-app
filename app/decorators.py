6bd5ec74a3a5f882ed2d640615158a425f0d84d0
apontive <apontive@mail.umw.edu>
2017-04-19 21:13:44 +0000

    remove repeated message from unconfirmed page

--- app/decorators.py
+++ app/decorators.py
@@ -6,7 +6,6 @@ def check_confirmed(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
         if current_user.is_confirmed is False:
-            flash('Please confirm your account!', 'warning')
             return redirect(url_for('auth.unconfirmed'))
         return func(*args, **kwargs)
     return decorated_function
